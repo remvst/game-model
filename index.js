4feadfdefe1fdd5b1b93c5a4cc6fd45400be1164
remvst <remvst@gmail.com>
2019-06-30 14:05:04 -0400

    No need to export this

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = {
     'Entity': require('./src/entity'),
     'Trait': require('./src/trait'),
-    'AgingTrait': require('./src/traits/aging-trait'),
     'World': require('./src/world'),
     'WorldEvent': require('./src/events/world-event'),
     'ObjectSet': require('./src/collections/object-set'),

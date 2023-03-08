3cf4a996c2e73aa6fbac1530f8a823850e688687
remvst <remvst@gmail.com>
2023-03-07 19:59:36 -0500

    Fixed crash when ID is falsy

--- src/adapt-id.ts
+++ src/adapt-id.ts
@@ -8,7 +8,7 @@ export default function adaptId(originalId: string, triggererId: string, world: 
         return triggererId;
     }
 
-    if (originalId.startsWith(TRAIT_PREFIX)) {
+    if (originalId?.startsWith(TRAIT_PREFIX)) {
         const traitKey = originalId.slice(TRAIT_PREFIX.length);
         for (const entity of world.entities.bucket(traitKey)) {
             return entity.id;
